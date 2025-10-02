7213d26e5f06c3cd3b7f6debb5eba757b071b64e
45tera <45terax@gmail.com>
2025-10-02 23:40:04 +0800

    feat: add thyme

--- src/main/java/com/example/cc_backend/HomeController.java
+++ src/main/java/com/example/cc_backend/HomeController.java
@@ -1,11 +1,18 @@
 package com.example.cc_backend;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("name", "tethra");
+        return "index.html";
+    }
+
+    @GetMapping("/home")
+    public String getHome() {
         return "index.html";
     }
 }
